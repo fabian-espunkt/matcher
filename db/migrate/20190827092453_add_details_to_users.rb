3660c005465419f4a690e897d4d9d6b178830c07
Fabian Schulze Wierling <fabian.schuwie@gmail.com>
2019-08-27 12:11:34 +0200

    fixed the bug that the schema doesn't accept arrays as data types and migrated. We have a clean schema now.

--- db/migrate/20190827092453_add_details_to_users.rb
+++ db/migrate/20190827092453_add_details_to_users.rb
@@ -1,22 +1,22 @@
 class AddDetailsToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column: :users, :type, :string
-    add_column: :users, :first_name, :string
-    add_column: :users, :last_name, :string
-    add_column: :users, :company_name, :string
-    add_column: :users, :position, :string
-    add_column: :users, :company_city, :string
-    add_column: :users, :firm_description, :string
-    add_column: :users, :stages, :array
-    add_column: :users, :sectors, :array
-    add_column: :users, :business_models, :array
-    add_column: :users, :selling_to, :array
-    add_column: :users, :company_url, :string
-    add_column: :users, :launch_status, :string
-    add_column: :users, :company_founding_year, :integer
-    add_column: :users, :generating_revenue, :boolean
-    add_column: :users, :profitable, :boolean
-    add_column: :users, :ticket_size, :string
+    add_column :users, :type, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :company_name, :string
+    add_column :users, :position, :string
+    add_column :users, :company_city, :string
+    add_column :users, :firm_description, :string
+    add_column :users, :stages, :string
+    add_column :users, :sectors, :string
+    add_column :users, :business_models, :string
+    add_column :users, :selling_to, :string
+    add_column :users, :company_url, :string
+    add_column :users, :launch_status, :string
+    add_column :users, :company_founding_year, :integer
+    add_column :users, :generating_revenue, :boolean
+    add_column :users, :profitable, :boolean
+    add_column :users, :ticket_size, :string
     # fraglich? profile picture, pitch_deck, company_logo
   end
 end
