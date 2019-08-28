7dc8681a182e48bf75d1ef4066bf84fe59f98115
Fabian Schulze Wierling <fabian.schuwie@gmail.com>
2019-08-28 16:41:59 +0200

    Added cloudinary configuration to our rails app

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  mount_uploader :photo, PhotoUploader
+
   has_many :attendances, dependent: :destroy
   has_many :availabilities, through: :attendances
   has_many :events, through: :attendances
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_28_102210) do
+ActiveRecord::Schema.define(version: 2019_08_28_143843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,8 @@ ActiveRecord::Schema.define(version: 2019_08_28_102210) do
     t.string "ticket_size_min"
     t.string "mobile"
     t.string "ticket_size_max"
+    t.boolean "admin"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
