fd41d09990f21dc94d9eb17c869297e851cf8f43
Fabian Schulze Wierling <fabian.schuwie@gmail.com>
2019-08-29 19:30:49 +0200

    Event meeting_points now selectable

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,5 +3,6 @@ class AppointmentsController < ApplicationController
     @appointments = policy_scope(Meeting)
     @sched_appointments = Meeting.all.where(matching_status: "Scheduled")
     @user_appointments = @sched_appointments.where(investor: current_user)
+    @events = Meeting.where(investor_id: current_user.id)
   end
 end
